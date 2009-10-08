74ce9c7fc9287c72be78668f3689dc50821d8f0c
ggironda <gabriel.gironda@centro.net>
2009-10-08 14:55:10 -0500

    Making the code even more unreadable

--- lib/smack.rb
+++ lib/smack.rb
@@ -1,14 +1,11 @@
 module Smack
   def self.init; Object.instance_eval { include Smack }; end
-  
   def smack_inject(substitutions)
     (class << self; self; end).instance_eval { @smack_subs = (@smack_subs || {}).merge!(substitutions) }
   end
-  
   def smack_get(klass_or_mod)
     [self, *(class << self; self; end).ancestors].any? { |mod| (found_mod = mod.smack_fetch(klass_or_mod)) ? (break(found_mod)) : nil } || klass_or_mod
   end
-  
   def smack_fetch(klass_or_mod)
     (class << self; self; end).instance_eval { @smack_subs ? @smack_subs[klass_or_mod] : nil }
   end
